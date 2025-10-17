41402b8636d094313ec8a2d3d0e71dc82293f798
David <atunwadavidb@gmail.com>
2025-10-17 21:28:53 +0100

    Verifies user input

--- app.py
+++ app.py
@@ -7,13 +7,12 @@ app = Flask(__name__)
 @app.route("/", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
-        if request.form.get("return-button") == "Return":
-            return redirect("/")
         num_list = []
         digit_span = int(request.form.get('dspan'))
         for _ in range(0, digit_span):
             num_list.append(random.randrange(0, 9))
-        return render_template('game_play.html', num_list=num_list)
+        num_sequence = "".join(map(str, num_list))
+        return render_template('game_play.html', num_list=num_list, num_sequence=num_sequence )
     else:
         return render_template('game_home.html')
 
